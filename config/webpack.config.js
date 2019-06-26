9d1850578bd6a85d3f0e0d05d3c1551e9ff0f45f
roughwin <roughwin@qq.com>
2019-06-26 14:00:56 +0800

    add babel plugins

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -353,6 +353,10 @@ module.exports = function(webpackEnv) {
                       },
                     },
                   ],
+                  [
+                    require('@babel/plugin-proposal-decorators'),
+                    { legacy: true }
+                  ],
                 ],
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
